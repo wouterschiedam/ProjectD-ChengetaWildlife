6ee0404f7a4d9baed57af9f07389a802dbf068db
Wouter <Wouterschiedam98@gmail.com>
2022-05-26 16:33:12 +0200

    Squashed commit of the following:
    commit e05b4d425555587309a6963d65e9664710cf4e7f
    Merge: e0641d1 3ae2c28
    Author: Esat-Aydin <esat61_2001@hotmail.com>
    Date:   Thu May 26 14:52:25 2022 +0200
    
        Merge branch 'Vue-login-+-dashboard' of https://github.com/wouterschiedam/ProjectD-ChengetaWildlife into Vue-login-+-dashboard
    
    commit e0641d1b5e0505c24345fc68a3983e79443e0682
    Author: Esat-Aydin <esat61_2001@hotmail.com>
    Date:   Thu May 26 14:52:18 2022 +0200
    
        upgrade
    
        dashboard map toegevoegd en fullscreen en printmap plus circle marker en boundries

--- Frontend/src/views/dashboard.vue
+++ Frontend/src/views/dashboard.vue
@@ -2,11 +2,22 @@
   <div class="dashboard-container">
     <app-header1 rootClassName="header1-root-class-name"></app-header1>
     <div class="dashboard-container1">
-      <div class="dashboard-map"></div>
+      <div class="dashboard-map" id="map">
+            <l-map style="height: 100%; Width: 100%;" :zoom="zoom" :center="center" :bounds ="bounds" :max-bounds="maxBounds">
+            <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
+            <l-Circle
+              :lat-lng="circle.center"
+              :radius="circle.radius"
+              :color="circle.color"
+            />
+            </l-map>
+      </div>
       <div class="dashboard-sidebar">
         <div class="dashboard-container2">
           <span class="dashboard-text">Dashboard</span>
-          <span class="dashboard-text1">Historische data</span>
+          <span class="dashboard-text" @click=fullScreenView()>Fullscreen</span>
+          <span class="dashboard-text" @click=printMapHtml()>Print map</span>
+          <span class="dashboard-text">Historische data</span>
         </div>
       </div>
       <div class="dashboard-geluidendata">
@@ -43,8 +54,10 @@
 
 <script>
 import AppHeader1 from '../components/header1'
-    import AppFooter from '../components/footer'
-    import axios from 'axios'
+import AppFooter from '../components/footer'
+import { latLngBounds, latLng } from "leaflet";
+import { LMap, LTileLayer, LMarker, LCircle } from 'vue2-leaflet';
+import html2canvas from 'html2canvas';
 
 export default {
   name: 'Dashboard',
@@ -74,8 +87,61 @@ export default {
         },
   components: {
     AppHeader1,
-/*    AppFooter,*/
+    AppFooter,
+    LMap,
+    LTileLayer,
+    LMarker,
+    LCircle
   },
+  data(){
+        return {
+            url: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+            attribution:
+                '&copy; <a target="_blank" href="http://osm.org/copyright%22%3EOpenStreetMap</a> contributors',
+            zoom: 16,
+            center: [51.505, -0.159],
+            bounds: latLngBounds([
+                  [-2.30081290280357, 23.16963806152345],
+                  [-2.82991732677597, 23.58716201782228]
+                ]),
+            maxBounds: latLngBounds([
+                  [-2.30081290280357, 23.16963806152345],
+                  [-2.82991732677597, 23.58716201782228]
+                ]),
+            Markers: [],
+            circle : {
+              center: [-2.45, 13.359],
+              radius: 1000,
+              color: '#ff7800'
+            },
+            circle: {
+              center: [-44.55, 23.34],
+              radius: 1118,
+              color: 'red'}
+        };
+  },
+    methods: {
+        fullScreenView() {
+            var mapId = document.getElementById('map');
+            mapId.requestFullscreen();
+        },
+        printMapHtml() {
+            const screenshotTarget = document.getElementById('map');
+            html2canvas(screenshotTarget, {
+                useCORS: true,
+                allowTaint: true
+            }).then((canvas) => {
+                const base64image = canvas.toDataURL("image/png");
+                var temp_link = document.createElement('a');
+                temp_link.href = base64image;
+                temp_link.download = "map.png";
+                temp_link.click();
+            });
+        }
+    },
+      mounted() {
+
+    },
   metaInfo: {
     title: 'dashboard - Chengeta wildlife',
     meta: [
@@ -138,7 +204,7 @@ th, td {
   top: 0px;
   flex: 0 0 auto;
   left: 200px;
-  width: 977px;
+  width: 100%;
   height: 517px;
   display: flex;
   position: absolute;
@@ -168,12 +234,16 @@ th, td {
   top: var(--dl-space-space-unit);
   left: 59px;
   align-self: center;
+  cursor: pointer;
+  color: black;
+  margin-bottom: 1rem;
 }
 .dashboard-text1 {
   top: var(--dl-space-space-tripleunit);
   left: 40px;
   position: absolute;
   align-self: center;
+  color: black;
 }
 .dashboard-geluidendata {
   flex: 0 0 auto;
