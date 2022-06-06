f103ea61474bf6d23142b7a4c9000747c34ea723
Wouter <Wouterschiedam98@gmail.com>
2022-06-06 15:54:37 +0200

    Squashed commit of the following:
    commit ef1761372f2b63d965658493cd36c9c3a4371232
    Author: Wouter <Wouterschiedam98@gmail.com>
    Date:   Sun Jun 5 15:50:28 2022 +0200
    
        added legend + color marker

--- Frontend/src/views/dashboard.vue
+++ Frontend/src/views/dashboard.vue
@@ -51,7 +51,6 @@
         <span class="material-icons-sharp">menu</span>
       </button>
     </div>
-  </div>
 </template>
 
 <script>
@@ -63,8 +62,7 @@ import { LMap, LTileLayer, LMarker, LPopup, LFeatureGroup } from "vue2-leaflet";
 import html2canvas from "html2canvas";
 import axios from "axios";
 import router from "../router";
-
-
+import IconMaterial from 'leaflet-iconmaterial'
 var VueCookie = require("vue-cookie");
 
 export default {
@@ -97,18 +95,7 @@ export default {
     };
   },
   methods: {
-    checkSession() {
-      var oauth = localStorage.getItem("token");
-      var bodyFormData = new FormData();
-      bodyFormData.append("oauth", oauth);
-      axios.post("api/auth/session", bodyFormData).then((response) => {
-        if (response.data.success) {
-          return true
-        } else {
-          return false;
-        }
-      });
-    },
+
     historyData() {
       this.$router.replace({name: "historyData"});
     },
@@ -163,12 +150,52 @@ export default {
         maxZoom: 18,
       }).addTo(map);
       this.AddMarkers(map);
+      var legend = L.control({position: 'topright'});
+      legend.onAdd = function (map) {
+          var div = L.DomUtil.create('div', 'info legend'),
+              grades = [0, 20, 40, 60, 80],
+              color = ['#ccccff', '#b2b2ff', '#9999ff', '#7f7fff', '#6666ff']
+
+          // Loop for color shades
+          for (var i = 0; i < grades.length; i++) {
+              div.innerHTML +=
+                  '<span style="background: ' + color[i] + '"></span> ';
+          }
+          // a line break
+          div.innerHTML += '<br>';
+          // second loop for text
+          for (var i = 0; i < grades.length; i++) {
+              div.innerHTML +=
+                  '<label>' + grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] : '+') + '</label>';
+          }
+          return div;
+      };
+      legend.addTo(map);
     },
     AddMarkers(map) {
       var MyMarkers = L.featureGroup();
       for (var i = 0; i < 13; i++) {
-        console.log(this.marker[i]);
-        var marker = new L.marker([this.marker[i][0], this.marker[i][1]]);
+        var date = new Date(this.Markers[i].time * 1000).toLocaleTimeString("en-NL").toString()
+        if (this.Markers[i].probability <= 20 && this.Markers[i].probability >= 0){
+          var marker = new L.marker([this.marker[i][0], this.marker[i][1]]).bindPopup("Time: "+ date +"\n, probability: "+(this.Markers[i].probability.toString())+"\n, Soundtype: "+ (this.Markers[i].soundtype.toString()));
+          // add color to markers
+        }
+        if (this.Markers[i].probability <= 40 && this.Markers[i].probability > 20){
+          var marker = new L.marker([this.marker[i][0], this.marker[i][1]]).bindPopup("Time: "+ date +"\n, probability: "+(this.Markers[i].probability.toString())+"\n, Soundtype: "+ (this.Markers[i].soundtype.toString()));
+          // add color to markers
+        }
+        if (this.Markers[i].probability <= 60 && this.Markers[i].probability > 40){
+          var marker = new L.marker([this.marker[i][0], this.marker[i][1]]).bindPopup("Time: "+ date +"\n, probability: "+(this.Markers[i].probability.toString())+"\n, Soundtype: "+ (this.Markers[i].soundtype.toString()));
+          // add color to markers
+        }
+        if (this.Markers[i].probability <= 80 && this.Markers[i].probability > 60){
+          var marker = new L.marker([this.marker[i][0], this.marker[i][1]]).bindPopup("Time: "+ date +"\n, probability: "+(this.Markers[i].probability.toString())+"\n, Soundtype: "+ (this.Markers[i].soundtype.toString()));
+          // add color to markers
+        }
+        if (this.Markers[i].probability <= 100 && this.Markers[i].probability > 80){
+          var marker = new L.marker([this.marker[i][0], this.marker[i][1]]).bindPopup("Time: "+ date +"\n, probability: "+(this.Markers[i].probability.toString())+"\n, Soundtype: "+ (this.Markers[i].soundtype.toString()));
+          // add color to markers
+        }
         MyMarkers.addLayer(marker);
       }
 
@@ -218,7 +245,6 @@ export default {
     ],
   },
   created() {
-    this.CheckValidSession();
 
     //reload every 60 seconds
     // const counter = setInterval(() => {
@@ -233,6 +259,19 @@ export default {
 </script>
 
 <style>
+/* LEGEND */
+.legend span, .legend label {
+  display: block;
+  width: 50px;
+  height: 18px;
+  float: left;
+  background-color: white;
+  padding: 5px;
+  text-align: center;
+  font-size: 100%;
+  color: black;
+
+}
 /* STYLING DATA */
 main {
   width: 50%;
@@ -240,148 +279,149 @@ main {
 
 
 .dashboard-map {
-  flex: 0 0 auto;
-  width: 100%;
-  height: 517px;
-  display: flex;
-  position: relative;
-  align-items: flex-start;
-  flex-direction: column;
+    flex: 0 0 auto;
+    width: 100%;
+    height: 517px;
+    display: flex;
+    position: relative;
+    align-items: flex-start;
+    flex-direction: column;
 }
 /* STYLING SIDEBAR */
 a {
-  color: var(--color-dark);
+    color: var(--color-dark);
 }
 img {
-  display: block;
-  width: 100%;
+    display: block;
+    width: 100%;
 }
 h1 {
-  font-weight: 800;
-  font-size: 1.8rem;
+    font-weight: 800;
+    font-size: 1.8rem;
 }
 h2 {
-  font-size: 1.4rem;
+    font-size: 1.4rem;
 }
 h3 {
-  font-size: 0.87rem;
+    font-size: 0.87rem;
 }
 h4 {
-  font-size: 0.8rem;
+    font-size: 0.8rem;
 }
 h5 {
-  font-size: 0.77rem;
+    font-size: 0.77rem;
 }
 small {
-  font-size: 0.75rem;
+    font-size: 0.75rem;
 }
 .container {
-  display: grid;
-  margin: 0 auto;
-  gap: 1.8rem;
-  grid-template-columns: 14rem 82% auto;
+    display: grid;
+    width: 98%;
+    margin: 0 auto;
+    gap: 1.8rem;
+    grid-template-columns: 14rem 82% auto;
 }
 aside {
-  height: 100vh;
-  background: var(--color-light);
+    height: 100vh;
+    background: var(--color-light);
 }
 aside .top {
-  background: white;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  margin-top: 1.4rem;
+    background: white;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-top: 1.4rem;
 }
 aside .logo {
-  display: flex;
-  gap: 0.8rem;
+    display: flex;
+    gap: 0.8rem;
 }
 aside .logo img {
-  width: 2.5rem;
-  height: 2.5rem;
+    width: 2.5rem;
+    height: 2.5rem;
 }
 aside .close {
-  display: none;
+    display: none;
 }
 aside .sidebar {
-  background: white;
-  display: flex;
-  flex-direction: column;
-  position: relative;
-  height: 86vh;
-  top: 3rem;
+    background: white;
+    display: flex;
+    flex-direction: column;
+    position: relative;
+    height: 86vh;
+    top: 3rem;
 }
 aside h3 {
-  font-weight: 600;
+    font-weight: 600;
 }
 aside .sidebar a {
-  display: flex;
-  color: var(--color-dark);
-  margin-left: 2rem;
-  gap: 1rem;
-  align-items: center;
-  position: relative;
-  height: 3.7rem;
-  transition: all 300ms ease;
-  cursor: pointer;
+    display: flex;
+    color: var(--color-dark);
+    margin-left: 2rem;
+    gap: 1rem;
+    align-items: center;
+    position: relative;
+    height: 3.7rem;
+    transition: all 300ms ease;
+    cursor: pointer;
 }
 aside .sidebar a span {
-  font-size: 1.6rem;
-  transition: all 300ms ease;
+    font-size: 1.6rem;
+    transition: all 300ms ease;
 }
 aside .sidebar a:last-child {
-  position: absolute;
-  bottom: 2rem;
-  width: 100%;
+    position: absolute;
+    bottom: 2rem;
+    width: 100%;
 }
 aside .sidebar a.active {
-  background: var(--color-light);
-  color: var(--color-primary);
-  margin-left: 0;
+    background: var(--color-light);
+    color: var(--color-primary);
+    margin-left: 0;
 }
 aside .sidebar a.active:before {
-  content: "";
-  width: 6px;
-  height: 100%;
-  background: var(--color-primary);
+    content: "";
+    width: 6px;
+    height: 100%;
+    background: var(--color-primary);
 }
 aside .side a.active span {
-  color: var(--color-primary);
-  margin-left: calc(1rem - 3px);
+    color: var(--color-primary);
+    margin-left: calc(1rem - 3px);
 }
 aside .sidebar a:hover {
-  color: var(--color-primary);
+    color: var(--color-primary);
 }
 aside .sidebar a:hover span {
-  margin-left: 1rem;
+    margin-left: 1rem;
 }
 /* TOP */
 .top {
-  display: none;
+    display: none;
 }
 
 @media screen and (max-width: 820px) {
-  .container {
-    width: 94%;
-    grid-template-columns: 10rem 88% auto;
-    margin-left: 1%;
-  }
-  aside .logo h2 {
-    display: none;
-  }
-  aside .sidebar h3 {
-    display: none;
-  }
-  aside .sidebar h3 {
-    width: 5.6rem;
-  }
-  aside .sidebar h3 {
-    position: relative;
-    margin-top: 1.8rem;
-  }
-  .dashboard-map {
-    width: 95%;
-  }
+    .container {
+        width: 94%;
+        grid-template-columns: 10rem 88% auto;
+        margin-left: 1%;
+    }
+    aside .logo h2 {
+        display: none;
+    }
+    aside .sidebar h3 {
+        display: none;
+    }
+    aside .sidebar h3 {
+        width: 5.6rem;
+    }
+    aside .sidebar h3 {
+        position: relative;
+        margin-top: 1.8rem;
+    }
+    .dashboard-map {
+        width: 95%;
+    }
 }
 @media screen and (max-width: 768px) {
   .container {
