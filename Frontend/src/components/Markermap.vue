3722a163baff831218d360378b4d08b4924bd6b1
MT0993790 <75487802+MT0993790@users.noreply.github.com>
2022-07-14 22:14:17 +0200

    Colored markers on first load and legend fix

--- Frontend/src/components/Markermap.vue
+++ Frontend/src/components/Markermap.vue
@@ -108,7 +108,7 @@
                         grades = ["gunshot", "vehicle", "animal", "unknown"],
                         color = [
                             "#FF0000",
-                            "#FFFF00",
+                            "#FFA500",
                             "#00993C",
                             "#6666ff",
                         ];
@@ -138,9 +138,8 @@
                     var date = new Date(this.Markers[i].time * 1000)
                         .toLocaleTimeString("en-NL")
                         .toString();
-
-
                     var template =
+                        '<div class="select">Status: <select v-model="value" @change="myFunction(value)"><option value="Open">open</option><option value="Closed">closed</option><option value="In progress">in progress</option> </select> </div >' +
                         "<div>Time: " + date + "</div>" +
                         "<div>Probability: " + this.Markers[i].probability.toString() + "</div>" +
                         "<div>Soundtype: " + this.Markers[i].soundtype.toString() + "</div>" +
@@ -149,10 +148,30 @@
                         this.Markers[i].probability <= 20 &&
                         this.Markers[i].probability >= 0
                     ) {
-                        var zerotwenty = L.icon({
-                            iconUrl: require("../markers/marker-svg.png"),
-                            iconSize: [25, 45],
-                        });
+                        if (this.Markers[i].soundtype == "vehicle") {
+                            var zerotwenty = L.icon({
+                                iconUrl: require("../markers/vehicle_zerotwenty.png"),
+                                iconSize: [25, 45],
+                            });
+                        }
+                        else if (this.Markers[i].soundtype == "animal") {
+                            var zerotwenty = L.icon({
+                                iconUrl: require("../markers/animal_zerotwenty.png"),
+                                iconSize: [25, 45],
+                            });
+                        }
+                        else if (this.Markers[i].soundtype == "gunshot") {
+                            var zerotwenty = L.icon({
+                                iconUrl: require("../markers/gunshot_zerotwenty.png"),
+                                iconSize: [25, 45],
+                            });
+                        }
+                        else {
+                            var zerotwenty = L.icon({
+                                iconUrl: require("../markers/marker-svg.png"),
+                                iconSize: [25, 45],
+                            });
+                        }
                         this.markers.push(new L.marker(
                             [this.marker[i][0], this.marker[i][1]],
                             { icon: zerotwenty }
@@ -162,57 +181,133 @@
                         this.Markers[i].probability <= 40 &&
                         this.Markers[i].probability > 20
                     ) {
-                        var twentyfourty = L.icon({
-                            iconUrl: require("../markers/twentyfourty.png"),
-                            iconSize: [25, 45],
-                        });
+                        if (this.Markers[i].soundtype == "vehicle") {
+                            var twentyfourty = L.icon({
+                                iconUrl: require("../markers/vehicle_twentyfourty.png"),
+                                iconSize: [25, 45],
+                            });
+                        }
+                        else if (this.Markers[i].soundtype == "animal") {
+                            var twentyfourty = L.icon({
+                                iconUrl: require("../markers/animal_twentyfourty.png"),
+                                iconSize: [25, 45],
+                            });
+                        }
+                        else if (this.Markers[i].soundtype == "gunshot") {
+                            var twentyfourty = L.icon({
+                                iconUrl: require("../markers/gunshot_twentyfourty.png"),
+                                iconSize: [25, 45],
+                            });
+                        }
+                        else {
+                            var twentyfourty = L.icon({
+                                iconUrl: require("../markers/twentyfourty.png"),
+                                iconSize: [25, 45],
+                            });
+                        }
                         this.markers.push(new L.marker(
                             [this.marker[i][0], this.marker[i][1]],
                             { icon: twentyfourty }
                         ));
-                        // add color to markers
                     }
                     if (
                         this.Markers[i].probability <= 60 &&
                         this.Markers[i].probability > 40
                     ) {
-                        var fourtysixty = L.icon({
-                            iconUrl: require("../markers/fourtysixty.png"),
-                            iconSize: [25, 45],
-                        });
+                        if (this.Markers[i].soundtype == "vehicle") {
+                            var fourtysixty = L.icon({
+                                iconUrl: require("../markers/vehicle_fourtysixty.png"),
+                                iconSize: [25, 45],
+                            });
+                        }
+                        else if (this.Markers[i].soundtype == "animal") {
+                            var fourtysixty = L.icon({
+                                iconUrl: require("../markers/animal_fourtysixty.png"),
+                                iconSize: [25, 45],
+                            });
+                        }
+                        else if (this.Markers[i].soundtype == "gunshot") {
+                            var fourtysixty = L.icon({
+                                iconUrl: require("../markers/gunshot_fourtysixty.png"),
+                                iconSize: [25, 45],
+                            });
+                        }
+                        else {
+                            var fourtysixty = L.icon({
+                                iconUrl: require("../markers/fourtysixty.png"),
+                                iconSize: [25, 45],
+                            });
+                        }
                         this.markers.push(new L.marker(
                             [this.marker[i][0], this.marker[i][1]],
                             { icon: fourtysixty }
                         ));
-                        // add color to marker
                     }
                     if (
                         this.Markers[i].probability <= 80 &&
                         this.Markers[i].probability > 60
                     ) {
-                        var sixtyeighty = L.icon({
-                            iconUrl: require("../markers/sixtyeighty.png"),
-                            iconSize: [25, 45],
-                        });
+                        if (this.Markers[i].soundtype == "vehicle") {
+                            var sixtyeighty = L.icon({
+                                iconUrl: require("../markers/vehicle_sixtyeighty.png"),
+                                iconSize: [25, 45],
+                            });
+                        }
+                        else if (this.Markers[i].soundtype == "animal") {
+                            var sixtyeighty = L.icon({
+                                iconUrl: require("../markers/animal_sixtyeighty.png"),
+                                iconSize: [25, 45],
+                            });
+                        }
+                        else if (this.Markers[i].soundtype == "gunshot") {
+                            var sixtyeighty = L.icon({
+                                iconUrl: require("../markers/gunshot_sixtyeighty.png"),
+                                iconSize: [25, 45],
+                            });
+                        }
+                        else {
+                            var sixtyeighty = L.icon({
+                                iconUrl: require("../markers/sixtyeighty.png"),
+                                iconSize: [25, 45],
+                            });
+                        }
                         this.markers.push(new L.marker(
                             [this.marker[i][0], this.marker[i][1]],
                             { icon: sixtyeighty }
                         ));
-
                     }
                     if (
                         this.Markers[i].probability <= 100 &&
                         this.Markers[i].probability > 80
                     ) {
-                        var eightyplus = L.icon({
-                            iconUrl: require("../markers/eightyplus.png"),
-                            iconSize: [25, 45],
-                        });
+                        if (this.Markers[i].soundtype == "vehicle") {
+                            var eightyplus = L.icon({
+                                iconUrl: require("../markers/vehicle_eightyplus.png"),
+                                iconSize: [25, 45],
+                            });
+                        }
+                        else if (this.Markers[i].soundtype == "animal") {
+                            var eightyplus = L.icon({
+                                iconUrl: require("../markers/animal_eightyplus.png"),
+                                iconSize: [25, 45],
+                            });
+                        }
+                        else if (this.Markers[i].soundtype == "gunshot") {
+                            var eightyplus = L.icon({
+                                iconUrl: require("../markers/gunshot_eightyplus.png"),
+                                iconSize: [25, 45],
+                            });
+                        }
+                        else {
+                            var eightyplus = L.icon({
+                                iconUrl: require("../markers/eightyplus.png"),
+                                iconSize: [25, 45],
+                            });
+                        }
                         this.markers.push(new L.marker(
                             [this.marker[i][0], this.marker[i][1]],
                             { icon: eightyplus }
                         ));
-                        // add color to markers
                     }
                     /*if (
                         this.Markers[i].soundtype == "vehicle"
@@ -238,6 +333,7 @@
 
                 for (var i = 0; i < 15; i++) {
                     var template =
+                        '<div class="select">Status: <select v-model:where="filter" @change="GetSounds"><option value="">open</option><option value="gunshot">closed</option><option value="probability">in progress</option> </select> </div >' +
                         "<div>Time: " + date + "</div>" +
                         "<div>Probability: " + this.sounds[i].probability.toString() + "</div>" +
                         "<div>Soundtype: " + this.sounds[i].soundtype.toString() + "</div>" +
@@ -310,7 +406,7 @@
                             [this.sounds[i].latitude, this.sounds[i].longitude],
                             { icon: twentyfourty }
                         ));
-                        // add color to markers
+                        //add color to markers
                     }
                     if (
                         this.sounds[i].probability <= 60 &&
@@ -344,7 +440,7 @@
                             [this.sounds[i].latitude, this.sounds[i].longitude],
                             { icon: fourtysixty }
                         ));
-                        // add color to markers
+                        //add color to markers
                     }
                     if (
                         this.sounds[i].probability <= 80 &&
@@ -378,7 +474,7 @@
                             [this.sounds[i].latitude, this.sounds[i].longitude],
                             { icon: sixtyeighty }
                         ));
-                        // add color to markers
+                        //add color to markers
                     }
                     if (
                         this.sounds[i].probability <= 100 &&
@@ -412,7 +508,7 @@
                             [this.sounds[i].latitude, this.sounds[i].longitude],
                             { icon: eightyplus }
                         ));
-                        // add color to markers
+                        //add color to markers
                     }
                     MyMarkers.addLayer(this.markers[i].bindPopup(template));
 
